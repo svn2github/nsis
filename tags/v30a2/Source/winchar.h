e32a180f562343c2baec29d184845e18975fcd16
kichik <kichik@212acab6-be3b-0410-9dea-997c60f758d6>
2013-12-25 04:56:49 +0000

    Tagging for release 3.0a2
    git-svn-id: http://svn.code.sf.net/p/nsis/code/NSIS@6424 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ tags/v30a2/Source/winchar.h
@@ -0,0 +1,56 @@
+/*
+ * winchar.h
+ * 
+ * This file is a part of NSIS.
+ * 
+ * Copyright (C) 1999-2013 Nullsoft and Contributors
+ * 
+ * Licensed under the zlib/libpng license (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ * Licence details can be found in the file COPYING.
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty.
+ */
+
+#ifndef INC_NSIS_WINCHAR
+#define INC_NSIS_WINCHAR
+#include "Platform.h"
+
+#define MAKEINTRESOURCEWINW(i) ( (WINWCHAR*) MAKEINTRESOURCEW(i) )
+
+int WinWStrICmpASCII(const WINWCHAR *a, const char *b);
+int WinWStrNICmpASCII(const WINWCHAR *a, const char *b, size_t n);
+#ifdef _WIN32
+inline size_t WinWStrLen(const WINWCHAR *s) { return wcslen((wchar_t*)s); }
+inline WINWCHAR* WinWStrCpy(WINWCHAR *d, const WINWCHAR *s) { return (WINWCHAR*)wcscpy((wchar_t*)d, (wchar_t*)s); }
+inline WINWCHAR* WinWStrNCpy(WINWCHAR *d, const WINWCHAR *s, size_t n) { return (WINWCHAR*)wcsncpy((wchar_t*)d, (wchar_t*)s, n); }
+inline int WinWStrCmp(const WINWCHAR *a, const WINWCHAR *b) { return wcscmp((wchar_t*)a, (wchar_t*)b); }
+inline WINWCHAR* WinWStrDupFromWinWStr(const WINWCHAR *s) { return (WINWCHAR*)wcsdup((wchar_t*)s); }
+inline WINWCHAR* WinWStrDupFromTChar(const wchar_t *s) { return WinWStrDupFromWinWStr((WINWCHAR*)s); }
+inline int WinWStrToInt(const WINWCHAR *s) { return _wtoi((wchar_t*)s); }
+#else // !_WIN32
+size_t WinWStrLen(const WINWCHAR *s);
+WINWCHAR* WinWStrCpy(WINWCHAR *d, const WINWCHAR *s);
+WINWCHAR* WinWStrNCpy(WINWCHAR *d, const WINWCHAR *s, size_t n);
+int WinWStrCmp(const WINWCHAR *a, const WINWCHAR *b);
+WINWCHAR* WinWStrDupFromWinWStr(const WINWCHAR *s);
+WINWCHAR* WinWStrDupFromTChar(const TCHAR *s);
+int WinWStrToInt(const WINWCHAR *s);
+#endif // ~_WIN32
+
+#ifdef _UNICODE
+inline WINWCHAR* WinWStrDupFromTChar(const TCHAR *s, unsigned int codepage) { return WinWStrDupFromTChar(s); }
+#endif
+
+
+#if 0
+WCHAR *wcsdup_fromansi(const char* s, unsigned int codepage = CP_ACP);
+#ifdef _UNICODE
+#define wcsdup_fromTchar(s, codepage) _wcsdup(s)      // codepage is not used in this mode
+#else
+#define wcsdup_fromTchar(s, codepage) wcsdup_fromansi(s, codepage)
+#endif
+#endif
+#endif // ~INC_NSIS_WINCHAR
